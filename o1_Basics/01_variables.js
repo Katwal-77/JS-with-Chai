6e834892ab3d9797f2d94394e60893cd32cab762
Inner_Demon <78010278+Katwal-77@users.noreply.github.com>
2024-02-23 17:27:56 +0000

    variables

--- o1_Basics/01_variables.js
+++ o1_Basics/01_variables.js
@@ -11,4 +11,4 @@ let accountState
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
-// prefer not to use var because of issue in block scope and functional scope
+// prefer not to use var because of issue in block scope and functional scope ;
