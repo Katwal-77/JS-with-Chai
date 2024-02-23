726a04d3f2202f15694a97b4e41672a37107aa6d
Inner_Demon <78010278+Katwal-77@users.noreply.github.com>
2024-02-23 16:55:26 +0000

    variable

---
+++ o1_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234432;
+let accountEmail = "Prem@jkfjejklk";
+var accountPassword = "kdkfjww3434";
+// accountCity = "kathmandu"; wthout declaring the varible it is possible, in js it automatically reserved the storage and it is not preferable
+
+accountEmail = "himalkatwal@gnail.com";
+accountPassword = "pekek234";
+accountCity = "Bhaktapur";
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// prefer not to use var because of issue in block scope and functional scope
