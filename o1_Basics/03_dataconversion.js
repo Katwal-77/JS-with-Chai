c99171850143c666df00c0b5dae311ffbbe3a918
Inner_Demon <78010278+Katwal-77@users.noreply.github.com>
2024-02-23 18:16:53 +0000

    dataconversion

---
+++ o1_Basics/03_dataconversion.js
@@ -0,0 +1,22 @@
+const score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn = "prem"
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// if 1=> true / if 0=false
+// if "" false
+// if "prem"=>true
+
+
+let someNumber = 77
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
